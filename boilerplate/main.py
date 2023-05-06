cd3f3c20368b030407b3e742b556fe3866ca5757
tikayz <kwetrus@yandex.ru>
2023-05-06 11:54:13 +0300

    metrics for cpu, ram and requests

--- boilerplate/main.py
+++ boilerplate/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI, APIRouter, Request
 import os
 import logging
 import time
+import psutil
 from typing import cast
 from logging import config
 from .utils.seed import seed_data
 from .utils.db import database, migrate
 from .controllers.user import users_router
 from fastapi.middleware import Middleware, cors
-from aioprometheus import Counter, MetricsMiddleware, REGISTRY, Summary
+from aioprometheus import Counter, MetricsMiddleware, REGISTRY, Summary, Gauge
 from aioprometheus.asgi.starlette import metrics
 from aioprometheus.pusher import Pusher
 
@@ -37,6 +38,8 @@ PUSH_GATEWAY_ADDR = "http://127.0.0.1:9091"
 pusher = Pusher("my-job", PUSH_GATEWAY_ADDR, grouping_key={"instance": "test-app"})
 app.state.users_events_counter = Counter("events", "Number of events.")
 app.state.response_time = Summary("response_time", "Response time")
+app.state.cpu_use = Gauge("CPU_USAGE", "CPU usage in %")
+app.state.ram_use = Gauge("RAM_USAGE", "RAM usage in %")
 app.add_middleware(MetricsMiddleware)
 app.add_route("/metrics", metrics)
 
@@ -48,9 +51,12 @@ async def log_requests(request: Request, call_next):
     response = await call_next(request)
 
     process_time = (time.time() - start_time) * 1000
+
     request.app.state.users_events_counter.inc({"path": request.scope["path"]})
     request.app.state.response_time.observe({"lat": request.scope["path"]}, process_time)
-    print(process_time)
+    request.app.state.cpu_use.set({"type": "cpu %"}, psutil.cpu_percent())
+    request.app.state.ram_use.set({"type": "ram %"}, psutil.virtual_memory().percent)
+
     client_ip = cast(str, request.client)
     logging.info(
         f"HTTP Request - {request.method} {request.url.path} - \n \
